0663f04ec2bf05c2d9d619b3df1307c2dc359d14
ahmar-husain <ahmar.husain@maersk.com>
2022-11-13 12:49:11 +0530

    Modlue

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,10 +1,10 @@
 
-module "function_app" {
+module "resource_group" {
   source                = "./../../modules/resource_group/"
   location          = var.location
   resource_group_name     = var.resource_group_name
 }
-module "function_app" {
+module "kye_vault" {
   source                = "./../../modules/key_vault/"
   keyvault_name          = var.keyvault_name
   tenant_id     = var.tenant_id
