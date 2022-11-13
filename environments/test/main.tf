015b2be846f24fc57b47018c2f10e939d44a7520
ahmar-husain <ahmar.husain@maersk.com>
2022-11-13 13:13:51 +0530

    Destroy key vault

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,15 +1,15 @@
 
 module "resource_group" {
-  source                = "./../../modules/resource_group/"
-  location          = var.location
-  resource_group_name     = var.resource_group_name
-}
-module "key_vault" {
-  source                = "./../../modules/key_vault/"
-  keyvault_name          = var.keyvault_name
-  tenant_id     = var.tenant_id
-  object_id=var.object_id
-  location = var.location
+  source              = "./../../modules/resource_group/"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  depends_on   = [module.resource_group]
 }
+#module "key_vault" {
+#  source              = "./../../modules/key_vault/"
+#  keyvault_name       = var.keyvault_name
+#  tenant_id           = var.tenant_id
+#  object_id           = var.object_id
+#  location            = var.location
+#  resource_group_name = var.resource_group_name
+#  depends_on          = [module.resource_group]
+#}
